934a70537947b7638f64b3e5f5db4f04e490b9a1
Edwin Salvador Soto Hernandez <230110358@itsoeh.edu.mx>
2024-12-05 13:54:28 -0600

    Colas y pilas

--- cursodsa/learning/Queues/queue_sandbox.py
+++ cursodsa/learning/Queues/queue_sandbox.py
@@ -1,43 +1,58 @@
-from Node import Node
+from Node_sandbox import Node
+import logging
 
-class Queue():
-    def __init__(self, head_node = None, tail_node = None, size = 0, max_size = None):
+class Queue:
+    def __init__(self, head_node=None, tail_node=None, size=0, max_size=None):
         self.head_node = head_node
         self.tail_node = tail_node
         self.size = size
         self.max_size = max_size
-        
+
+    def get_size(self):
+        return self.size
+
+    def has_space(self):
+        if(self.max_size is None):
+            return True
+        else:
+            if(self.max_size > self.get_size()):
+                return True
+            else:
+                return False
+
+    def is_empty(self):
+        return self.size == 0
+    
     def peek(self):
         if(self.is_empty()):
             print("¡No hay nada que ver aquí!")
         else:
-            return self.head_node.value
-
-    def get_size(self): #Devolverá el valor de la propiedad size
-        return self.size
-        
-    def has_space(self): #Devolverá True si la cola tiene espacio para otro nodo.
-        if(self.max_size == None):
-            return True
-        elif(self.max_size > self.get_size()):
-            return True
-        else:
-            return False
-            
-    def is_empty(self): #Devolverá True si el tamaño es 0.
-        if(self.size == 0):
-            return True
-
+            return self.head_node.get_value()
+    
     def enqueue(self, value):
         if(self.has_space()):
             item_to_add = Node(value)
-            print("¡Agregando " + str(item_to_add.get_value()) + " a la cola!")
+            print(f"¡Agregando {item_to_add.get_value()} a la cola!")
             if(self.is_empty()):
                 self.head_node = item_to_add
                 self.tail_node = item_to_add
             else:
-                item_to_add = self.next_node
+                self.tail_node.set_next_node(item_to_add)
                 self.tail_node = item_to_add
             self.size += 1
         else:
-            print("¡Lo sentimos, no hay más espacio!")
+            logging.warning("¡Lo sentimos, no hay más espacio!")
+
+    def dequeue(self):
+        item_to_remove = Node(None)
+        if(not self.is_empty()):
+            item_to_remove = self.head_node
+            logging.info("¡Eliminando" + str(item_to_remove.get_value()) + "de la cola!")
+            if(self.get_size() == 1):
+                self.head_node = None
+            else:
+                self.head_node = self.head_node.get_next_node()
+            self.size = self.size - 1
+        else:
+            logging.info("¡Esta cola está totalmente vacía!")
+        return item_to_remove.get_value()
